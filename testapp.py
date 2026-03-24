f7b0f5b0257476eaceb3998f9aea543643c5c4bc
Nuha <125197197+nuhadasthagir@users.noreply.github.com>
2026-03-24 12:13:38 +0530

    Delete testapp.py

--- testapp.py
+++
@@ -1,7 +0,0 @@
-# st_test.py
-import streamlit as st
-st.set_page_config(page_title="Streamlit Test")
-st.title("Streamlit Render Test ✅")
-st.write("If you see this, Streamlit UI is rendering correctly.")
-if st.button("Say hello"):
-    st.write("Hello from Streamlit!")
